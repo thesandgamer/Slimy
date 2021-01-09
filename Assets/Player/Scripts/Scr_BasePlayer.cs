93ce0c3af86ff9938a6c0ad61530daab44fb8adc
Thesandgamer <ultraadri@free.fr>
2021-01-09 19:07:21 +0100

    Add movement and character controller
    rajout du movement du joueur et d'un character controller 2d récupéré sur la chaine de Brakeys

--- Assets/Player/Scripts/Scr_BasePlayer.cs
+++ Assets/Player/Scripts/Scr_BasePlayer.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Scr_BasePlayer : MonoBehaviour
 {
-    [Range(1, 10)]
-    public float walkSpeed = 5;
-
-    void Update()
-    {
-        //Déplacement
-        float value = Input.GetAxis("Horizontal");
-
-        Debug.Log(value);
-    }
+  
 
 }
---
+++ Assets/Player/Scripts/Scr_Player_Move.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Player_Move : MonoBehaviour
+{
+    [Range(1, 50)]
+    public float walkSpeed = 40;
+
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * walkSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false,false);
+    }
+}
